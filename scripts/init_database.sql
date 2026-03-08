fc8d3c126b179c221cd79a35c40842b0c6569ca5
Lan-Anh-12 <lzltop1lzl@gmail.com>
2026-03-08 10:42:11 +0700

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,14 @@
+USE master;
+CREATE DATABASE DataWarehouse;
+
+-- create database
+USE DataWarehouse;
+GO
+
+-- create schema
+CREATE SCHEMA bronze; 
+GO
+CREATE SCHEMA silver; 
+GO
+CREATE SCHEMA gold; 
+
